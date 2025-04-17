c00d83f4111569fd7fdcdf13fc4409ae18a2216e
vedasahithi <sahithivellanki@gmail.com>
2025-04-17 11:18:08 +0530

    initial commit

---
+++ test1.py
@@ -0,0 +1,12 @@
+import unittest
+from main import to_upper
+
+class MyTestCase(unittest.TestCase):
+    def test_to_upper(self):
+        name = "Yasmeen"
+        upper_name = to_upper(name)
+        self.assertEqual(upper_name, "YASMEEN")
+
+if __name__ == '__main__':
+    unittest.main()
+
